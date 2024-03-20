af71f84c4091cdf4a2b5ab14953181c4b5bd4020
jayshahscode <128133296+jayshahscode@users.noreply.github.com>
2024-03-20 11:16:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.Image. fromURL ('BirthdayImage.jpg', function (Img) {
+        block_
+        image.
+        object = Img;
+        block_image_object.scaleTowidth (700); block_image_object.scaleToHeight (510);
+        block_image_object.set({ 
+            top: 0,
+             left:o
+        });
+        canvas.add(block_image_object);
+        });
+	
+}
+
+function playSound()
+{
+	x.play () ;
+}
